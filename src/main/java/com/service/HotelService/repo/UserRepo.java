01eb3f7796378b8992c696f21eb5c7c331cec589
Li <aaa111334@gmail.com>
2025-03-26 14:49:32 +0800

    add user function

---
+++ src/main/java/com/service/HotelService/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.service.HotelService.repo;
+
+import com.service.HotelService.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+ Optional<User> findFirstByEmail(String email);
+
+}
---
+++ src/main/java/com/service/HotelService/utill/JwtUtill.java
@@ -0,0 +1,62 @@
+package com.service.HotelService.utill;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtill {
+
+    private String generateToken(Map<String,Object> extraClaims, UserDetails details){
+    return Jwts.builder().setClaims(extraClaims).setSubject(details.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
+            .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private Claims extractAllClaims (String token){
+        return Jwts.parser().setSigningKey(getSignKey()).build().parseClaimsJwt(token).getBody();
+    }
+
+    public String extractUserName(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T>claimsResolvers){
+    final Claims claims = extractAllClaims(token);
+    return claimsResolvers.apply(claims);
+    }
+
+    private Key getSignKey(){
+        byte[] keyBytes = Decoders.BASE64.decode("VGVzdA==");
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
